d5ace3270dc181fa9cd090ea706f77aeb8aee034
Ella Tanttu <ellajtanttu@gmail.com>
2021-12-07 16:33:50 -0800

    add files at end of day
    Co-authored-by: Kate Kiatsiri <keidsiri@gmail.com>
    Co-authored-by: Liz Thomas <thomas.elizabeth.k@gmail.com>

--- Palindrome.Tests/ModelTests/PalindromesTests.cs
+++ Palindrome.Tests/ModelTests/PalindromesTests.cs
@@ -1,22 +1,34 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Palindrome;
+using System.Collections.Generic;
 
 namespace Palindrome.Tests
 {
   [TestClass]
   public class PalindromesTests
   {
+      // [TestMethod]
+      // public void PalindromeTester_GetBool_SameStringAsInputted()
+      // {
+      //   Palindromes testString = new Palindromes();
+      //   Assert.AreEqual("lunch", testString.PalindromeTester("lunch"));
+      // }
       [TestMethod]
-      public void PalindromeTester_GetStringInput_SameStringAsInputted()
+      public void PalindromeTester_StringArray_True()
+      //NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
       {
         Palindromes testString = new Palindromes();
-        Assert.AreEqual("lunch", testString.PalindromeTester("lunch"));
+        Assert.AreEqual( true , testString.PalindromeTester("lunch"));
         // LeapYear testLeapYear = new LeapYear();
         // string testString = "stringnirts";
         // Assert.AreEqual("stringnirts", PalindromeTester("stringnirts"));
       }
   }
 }
+
+// > string[] fruits = { "apples", "oranges", "bananas", "pears" };
+// > fruits
+// string[4] { "apples", "oranges", "bananas", "pears" }
 
 /*
   using Microsoft.VisualStudio.TestTools.UnitTesting;
